ecb90c31a251ec1426692cb95e60af14d2d18fda
rampler <rampler@gmail.com>
2014-11-30 18:21:55 +0100

    Baron show/update to api 2 change.

--- Baron-Server/src/main/java/pl/foodiary/Application.java
+++ Baron-Server/src/main/java/pl/foodiary/Application.java
@@ -19,6 +19,7 @@ import java.util.Scanner;
 public class Application {
 
 	public static String mainAPI = "";
+	public static String mainAPI2 = "";
 
 	public static void main(String[] args) throws Exception {
 		//TODO to erase in final version
@@ -28,6 +29,12 @@ public class Application {
 			mainAPI += in.nextLine();
 		}
 		in.close();
+		File mainScreen2 = new File("mainScreen2.baronapi");
+		Scanner in2 = new Scanner(mainScreen2);
+		while (in2.hasNext()) {
+			mainAPI2 += in2.nextLine();
+		}
+		in2.close();
 		//TODO END to erase in final version
 
 		SpringApplication.run(Application.class, args);
