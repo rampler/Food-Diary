15191642f02d881cbb6f0727cf66687e4f92a859
rampler <rampler@gmail.com>
2014-12-03 14:10:22 +0100

    Added /statistics/counters

--- Baron-Server/src/main/java/pl/foodiary/repositories/IngredientRepository.java
+++ Baron-Server/src/main/java/pl/foodiary/repositories/IngredientRepository.java
@@ -1,10 +1,14 @@
 package pl.foodiary.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import pl.foodiary.domain.Ingredient;
 import pl.foodiary.domain.Meal;
+import pl.foodiary.domain.Product;
+import pl.foodiary.domain.User;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -12,5 +16,6 @@ import java.util.UUID;
  */
 @Repository
 public interface IngredientRepository extends PagingAndSortingRepository<Ingredient, UUID> {
+	long countByMeal(Meal meal);
 	Iterable<Ingredient> findByMeal(Meal meal);
 }
